dbae6752092f21200f7cf9bdfa08fd2c56f91d9c
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-25 11:23:16 -0700

    Added tokens to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :animes
+  has_secure_password
+  validates :password, length: {minimum: 6}
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::Email_REGEXP}
+  validates :username, presence: true, uniqueness: true
 end
