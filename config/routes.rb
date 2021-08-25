e00b06fca4191a41bcf3df4f4fdfd9c201283b6b
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-25 11:54:19 -0700

    routes done, need testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   resources :animes
   resources :categories
-  resources :users
+
+  resources :categories do
+    resources :animes
+  end
+
 
   post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :users, only: :create
+  get '/anime/:anime_id/categories/:id', to: 'categories#add_to_anime'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
