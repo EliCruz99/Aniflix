0464b743b4dca6b62a5c9acacb4098bca520b302
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-25 18:00:47 -0700

    basic FE file structure done

---
+++ client/src/screens/AllAnime/AllAnime.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AllAnime() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ client/src/screens/AnimeCreate/AnimeCreate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AnimeCreate() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
