cab64ba2f7f9885ad5bf34ecccba08047d2247d8
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-26 21:36:14 -0700

    Home renders base information

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Home() {
+export default function Home({allAnimes}) {
   return (
     <div>
-      
+      {allAnimes.map((anime) => {
+        return (
+          <Link to={`/anime/${anime.id}`}><p className="">{anime.name}</p></Link>
+        )
+      })}
     </div>
   )
 }
