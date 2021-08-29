12cf37c9c72e475be0fcf3ca03be3d5a061e08ba
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-29 14:46:39 -0700

    Tried to get the edit page to look like detail page

--- client/src/screens/AnimeEdit/AnimeEdit.jsx
+++ client/src/screens/AnimeEdit/AnimeEdit.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useParams } from 'react-router'
-
+import { useHistory } from 'react-router'
 
 export default function AnimeEdit({ allAnimes, handleDelete, handleUpdate, allCategories }) {
   const [formData, setFormData] = useState({
@@ -13,6 +13,7 @@ export default function AnimeEdit({ allAnimes, handleDelete, handleUpdate, allCa
   })
   const { name, description, img_url, watch_link, category_id } = formData
   const { id } = useParams()
+  const history = useHistory()
   
   useEffect(() => {
     const prefillFormData = () => {
@@ -38,26 +39,17 @@ export default function AnimeEdit({ allAnimes, handleDelete, handleUpdate, allCa
   }
 
   return (
-    <div>
-      <form
-        className="flex flex-col"
-        onSubmit={(e) => {
-        e.preventDefault()
-        handleUpdate(id,formData)
-      }}>
-        <label className="">
-          <h3 className="">Name</h3>
-          <input
-            type="text"
-            className="border"
-            name='name'
-            value={name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label className="">
-          <h3 className="">Image URL</h3>
+    <div className='container mx-auto'>
+        <div className="flex mb-28 mt-20">
+      <label className="flex flex-col mx-auto">
+        <img src={img_url} alt={name} className='shadow-lg w-72 h-96' />
+        <h3 className="">Image URL</h3>
+        <form
+          className=""
+          onSubmit={(e) => {
+            e.preventDefault()
+            handleUpdate(id, formData)
+          }}>
           <input
             type="text"
             className="border"
@@ -65,19 +57,26 @@ export default function AnimeEdit({ allAnimes, handleDelete, handleUpdate, allCa
             value={img_url}
             onChange={handleChange}
           />
+        </form>
         </label>
+      <form
+        className="flex flex-col px-64 justify-start mx-auto"
+        onSubmit={(e) => {
+        e.preventDefault()
+        handleUpdate(id,formData)
+      }}>
         <label className="">
-          <h3 className="">Watch Link</h3>
           <input
             type="text"
-            className="border"
-            name='watch_link'
-            value={watch_link}
+            className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl whitespace-nowrap text-left tracking-wide"
+            name='name'
+            value={name}
             onChange={handleChange}
+            required
           />
         </label>
         <label className="">
-          <select name="category_id" value={category_id} className="border" onChange={handleChange} required>
+          <select name="category_id" value={category_id} className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl items-baseline tracking-wide w-full" onChange={handleChange} required>
             <option value="category">Genre</option>
             {allCategories?.map((cat) => {
               return (
@@ -85,18 +84,36 @@ export default function AnimeEdit({ allAnimes, handleDelete, handleUpdate, allCa
               )
             })}
           </select>
-        </label>
+          </label>
         <label className="">
-          <h3 className="">Description</h3>
+          <h3 className="">Watch Link</h3>
+          <input
+            type="text"
+            className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl items-baseline tracking-wide"
+            name='watch_link'
+            value={watch_link}
+            onChange={handleChange}
+          />
+        </label>
+          </form>
+      </div>
+      <form
+        className="flex flex-col"
+        onSubmit={(e) => {
+        e.preventDefault()
+        handleUpdate(id,formData)
+      }}>
+        <label className="flex justify-start bg-palette-lightCard">
+          <h3 className="mx-28 text-palette-fontOnLight  text-4xl text-left">Description</h3>
+        </label>
           <textarea
             type="text"
-            className="border"
+            className="flex px-28 text-left bg-palette-lightCard text-xl justify-start resize-none"
             name='description'
             value={description}
             onChange={handleChange}
           />
-        </label>
-        <button className="border">Edit!</button>
+        <button className="" onClick={() => history.push(`anime/${id}`)}>Save</button>
         <button className="" onClick={() => handleDelete(id)}>Delete</button>
       </form>
     </div>
