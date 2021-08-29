a71d1669167de023c3586c93674c6589bbec3a3f
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-29 14:46:15 -0700

    almost done with styling also

--- client/src/screens/AnimeDetail/AnimeDetail.jsx
+++ client/src/screens/AnimeDetail/AnimeDetail.jsx
@@ -10,7 +10,6 @@ export default function AnimeDetail({currentUser}) {
   useEffect(() => {
     const anime = async () => {
       const singleAnime = await getAnime(id)
-      console.log(singleAnime)
       setCurrentAnime(singleAnime)
     }
     anime()
@@ -18,23 +17,25 @@ export default function AnimeDetail({currentUser}) {
 
   const userCreatedAnime = () => {
     if (currentUser?.id === currentAnime?.user_id) {
-      return <Link to={`/anime/${currentAnime?.id}/edit`} className='flex justify-end'><button>Edit</button></Link>
+      return <button className='bg-palette-card text-palette-font text-2xl mb-10'><Link to={`/anime/${currentAnime?.id}/edit`} className=''>Edit</Link></button>
     }
   }
 
   return (
-    <div className='flex flex-col justify-center content-center'>
-      <div className=" bg-gray-400 flex">
-      <img src={currentAnime?.img_url} alt={currentAnime?.name} className="flex max-w-lg" />
-      <div className="bg-red-500">
-      {userCreatedAnime()}
-      <p className="flex justify-end">{currentAnime?.name}</p>
-      <p className="flex justify-end">{currentAnime?.category.name}</p>
-      <a href={currentAnime?.watch_link} target="_blank" className="flex justify-end"><button className="border">WatchNow!</button></a>
+    <>
+      <div className='flex pb-28 pt-20 '>
+        <img src={currentAnime?.img_url} alt={currentAnime?.name} className=" shadow-lg w-72  h-96 mx-auto"/>
+        <div className="flex flex-col px-64 justify-start mx-auto">
+          {userCreatedAnime()}
+          <p className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl whitespace-nowrap text-left tracking-wide">{currentAnime?.name}</p>
+          <p className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl items-baseline tracking-wide">{currentAnime?.category.name}</p>
+          <a href={currentAnime?.watch_link} target="_blank" className="shadow text-2xl text-white bg-palette-darkCard whitespace-nowrap "><button className="w-40">Watch Now!</button></a>
+        </div>
       </div>
+      <div className="flex flex-col justify-start bg-palette-lightCard">
+        <p className="mx-28 text-palette-fontOnLight  text-4xl text-left"><span className='pl-32'>Description</span></p>
+        <p className="flex px-28 text-left text-palette-fontOnLight text-xl justify-start"><span className='pl-32 mb-4'>{currentAnime?.description}</span></p>
       </div>
-      <p className="">{currentAnime?.description}</p>
-      <div className="bg-purple-600 flex justify-center">p</div>
-    </div>
+    </>
   )
 }
