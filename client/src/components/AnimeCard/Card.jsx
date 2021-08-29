c097ee9f53d222f48baec87e1e146d52c4ba7e32
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-29 14:45:08 -0700

    Created cards to hold anime for category screen

---
+++ client/src/components/AnimeCard/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Card({anime}) {
+
+  const categoryName = ''
+
+  return (
+    <div className='flex w-72 h-32'>
+      <img src={anime.img_url} alt={anime.name} className="border-r-2 w-32 h-32" />
+      <div className="flex flex-col">
+        <span className=" border-b-2">{anime.name}</span>
+        <p className="border-b-2">{anime.category.name}</p>
+        <p className="overflow-hidden text-left overflow-ellipsis">{anime.description}</p>
+      </div>
+    </div>
+  )
+}
