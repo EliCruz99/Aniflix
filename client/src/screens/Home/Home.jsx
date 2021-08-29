41176478a77cb06feff2267f8d17650da549d8f1
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-29 14:47:02 -0700

    working on card element

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Card from '../../components/AnimeCard/Card'
 
-export default function Home({allAnimes}) {
+export default function Home({ allAnimes }) {
+  
+  console.log(allAnimes)
   return (
     <div>
       {allAnimes.map((anime) => {
         return (
-          <Link to={`/anime/${anime.id}`}><p className="">{anime.name}</p></Link>
+          <Link to={`/anime/${anime.id}`}><Card anime={anime}/></Link>
         )
       })}
     </div>
