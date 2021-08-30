ca7fd5797cd2981d67039fae6b27ed8ce5bc181b
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-30 06:12:17 -0700

    created a card to hold anime

--- client/src/components/AnimeCard/Card.jsx
+++ client/src/components/AnimeCard/Card.jsx
@@ -5,11 +5,11 @@ export default function Card({anime}) {
   const categoryName = ''
 
   return (
-    <div className='flex w-72 h-32'>
+    <div className='flex w-72 h-32 border-2'>
       <img src={anime.img_url} alt={anime.name} className="border-r-2 w-32 h-32" />
-      <div className="flex flex-col">
+      <div className="flex flex-col ">
         <span className=" border-b-2">{anime.name}</span>
-        <p className="border-b-2">{anime.category.name}</p>
+        <p className="border-b-2">{anime.category?.name}</p>
         <p className="overflow-hidden text-left overflow-ellipsis">{anime.description}</p>
       </div>
     </div>
