1afa6fd057acc310b1c413910cf4ffbed23d9342
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-30 07:02:11 -0700

    Category page is done. may add pagination

--- client/src/screens/AnimeCategory/AnimeCategory.jsx
+++ client/src/screens/AnimeCategory/AnimeCategory.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Card from '../../components/AnimeCard/Card'
+import { useState } from 'react'
 
-export default function AnimeCategory() {
+export default function AnimeCategory({allCategories, allAnimes}) {
+  const [category, setCategory] = useState('All')
+  
+  const handleChange = (e) => {
+    setCategory(e.target.value)
+    console.log(category)
+  }
+  
+  const categories = allCategories.map((cat) => <option value={cat.name}>{cat.name}</option> )
+  const filtered = allAnimes.filter((anime) => anime.category.name === category)
+  const shows = filtered.map((anime) => (<Link key={anime.id} to={`/anime/${anime.id}`} className='m-10'>{<Card anime={anime}/>}</Link>))
+
+  const categoryToAnime = () => {
+    if (category === 'All') {
+      return allAnimes.map((anime) => <Link key={anime.id} to={`/anime/${anime.id}`} className='m-10'>{<Card anime={anime}/>}</Link> )
+    }
+    return (filtered && shows)
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <select value={category} onChange={handleChange}>
+        <option>All</option>
+        {categories}
+      </select>
+      <div className='flex flex-wrap'>
+        {categoryToAnime()}
+      </div>
+      </>
   )
 }
