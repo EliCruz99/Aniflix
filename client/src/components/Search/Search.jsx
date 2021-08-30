2a39e148a336681ac922c16b0684d4c793df3519
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-30 06:15:34 -0700

    working on site wide search bar

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getAllAnime } from '../../services/anime'
 
 export default function Search() {
+  const [query, setQuery] = useState({ show: '' })
+  const [searchResults, setSearchResults] = useState([])
+  const [allAnimes, setAllAnimes] = useState([])
+  
+
+
+  useEffect(() => {
+    const fetchAnime = async() => {
+      const animeList = await getAllAnime()
+      setAllAnimes(animeList)
+    }
+    fetchAnime()
+  }, [])
+
+  const handleChange = (e) => {
+    const { name, value } = e?.target
+    setQuery((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    const results = allAnimes?.filter((anime) => {
+      anime.name.toLowerCase().includes(e?.target.value.toLowerCase())
+    })
+    setQuery(results)
+    setSearchResults(results)
+    console.log(results)
+  }
+
+
+  const handleSubmit = (e) => {
+  e.preventDefault()
+}
+
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <form className='' onSubmit={handleSubmit}>
+        <input type="text" name={query.show} value={query.show} placeholder='Search Anime...' onChange={handleSearch} className="w-full h-full text-12 text-blue-400 rounded" />
+      </form>
+      {searchResults.map((anime) => { <p key={anime.id}>{anime.name}</p>})}
+    </>
   )
 }
