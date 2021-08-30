f2e5694c0329b844782fb4f7bfb1a58cabe3c720
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-30 07:01:38 -0700

    added new routing for category

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -9,6 +9,7 @@ export default function Nav(props) {
   <div className='z-10 bg-palette-darkCard shadow-lg'>
       <nav className='flex flex-row justify-evenly flex-nowrap'>
         <Link className='mr-1 text-palette-font text-2xl' to='/home'>Aniflix</Link>
+        <Link to='/category' className='ml-1 text-palette-font'>Categories</Link>
       {currentUser ? (
         <div className='flex text-palette-font'>
           <Link to={`/profile/${currentUser.username}`} className="mr-5 "><p className="mr-1">{currentUser.username}</p></Link>
