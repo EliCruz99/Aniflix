b4089cc0980a8176b54285f44a59ab6a7fb33d99
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 12:51:45 -0700

    footer with linked/github

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 export default function Footer() {
   return (
-    <footer className='fixed bottom-0 flex justify-center w-screen'>
-      <h1 className="">footer</h1>
+    <footer className='fixed bottom-0 mt-0 flex flex-row justify-center w-screen'>
+      <h1 className="mr-4">created by Elijah Cruz</h1>
+      <a href="https://github.com/EliCruz99" target="_blank" rel="noreferrer" >
+        <FontAwesomeIcon icon={faGithub} className="mr-2 text-xl" />
+      </a>
+      <a href="https://www.linkedin.com/in/elijah-cruz" target="_blank" rel="noreferrer" >
+        <FontAwesomeIcon icon={faLinkedin} className="ml-2 text-xl" />
+       </a>
+
     </footer>
   )
 }
