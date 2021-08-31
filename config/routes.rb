eb9e90cccf73685f1bfe391a88964336d14c6b1f
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 09:25:06 -0700

    fixed a route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/auth/verify', to: 'authentication#verify'
   resources :users, only: [:create, :show]
   resources :categories, only: [:show, :index]
+  get '/anime/shuffle', to: 'animes#shuffle'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
