41e620b5c0fdc8d53ef88ee5e59df721f1e077e5
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 09:25:55 -0700

    now it looks like the edit/details page

--- client/src/screens/AnimeCreate/AnimeCreate.jsx
+++ client/src/screens/AnimeCreate/AnimeCreate.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { useHistory } from 'react-router'
 
 export default function AnimeCreate({handleCreate, allCategories}) {
   const [formData, setFormData] = useState({
@@ -10,6 +11,7 @@ export default function AnimeCreate({handleCreate, allCategories}) {
     category_id: ''
   })
   const { name, description, img_url, watch_link, category_id } = formData
+  const history = useHistory()
   
 
   const handleChange = (e) => {
@@ -19,27 +21,23 @@ export default function AnimeCreate({handleCreate, allCategories}) {
       [name]: value
     }))
   }
+  const handleSubmit = () => {
+    handleCreate(formData)
+    history.push('/home')
+  }
+
   return (
-    <div>
-      <form
-        className="flex flex-col"
-        onSubmit={(e) => {
-        e.preventDefault()
-        handleCreate(formData)
-      }}>
-        <label className="">
-          <h3 className="">Name</h3>
-          <input
-            type="text"
-            className="border"
-            name='name'
-            value={name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label className="">
-          <h3 className="">Image URL</h3>
+    <div className='container mx-auto'>
+        <div className="flex mb-28 mt-20">
+      <label className="flex flex-col mx-auto">
+        <img src={img_url} alt={name} className='shadow-lg w-72 h-96' />
+        <h3 className="">Image URL</h3>
+        <form
+          className=""
+          onSubmit={(e) => {
+            e.preventDefault()
+            handleCreate(formData)
+          }}>
           <input
             type="text"
             className="border"
@@ -47,38 +45,64 @@ export default function AnimeCreate({handleCreate, allCategories}) {
             value={img_url}
             onChange={handleChange}
           />
+        </form>
         </label>
+      <form
+        className="flex flex-col px-64 justify-start mx-auto"
+        onSubmit={(e) => {
+        e.preventDefault()
+        handleCreate(formData)
+      }}>
         <label className="">
-          <h3 className="">Watch Link</h3>
           <input
             type="text"
-            className="border"
-            name='watch_link'
-            value={watch_link}
+            className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl whitespace-nowrap text-left tracking-wide"
+            name='name'
+            value={name}
             onChange={handleChange}
+            required
           />
         </label>
         <label className="">
-          <select name="category_id" value={category_id} className="border" onChange={handleChange} required>
+          <select name="category_id" value={category_id} className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl items-baseline tracking-wide w-" onChange={handleChange} required>
             <option value="category">Genre</option>
-            {allCategories.map((cat) => {
+            {allCategories?.map((cat) => {
               return (
                 <option key={cat.id} name='category' value={Number(cat.id)}>{cat.name}</option>
               )
             })}
           </select>
-        </label>
+          </label>
         <label className="">
-          <h3 className="">Description</h3>
+          <h3 className="">Watch Link</h3>
+          <input
+            type="text"
+            className="shadow bg-palette-lightCard text-palette-fontOnDark mb-10 text-2xl items-baseline tracking-wide"
+            name='watch_link'
+            value={watch_link}
+            onChange={handleChange}
+          />
+        </label>
+          </form>
+      </div>
+      <form
+        className="flex flex-col"
+        onSubmit={(e) => {
+        e.preventDefault()
+        handleCreate(formData)
+      }}>
+        <label className="flex justify-start bg-palette-lightCard">
+          <h3 className="mx-28 text-palette-fontOnLight  text-4xl text-left">Description</h3>
+        </label>
           <textarea
             type="text"
-            className="border"
+            className="flex px-28 text-left bg-palette-lightCard text-xl justify-start resize-none"
             name='description'
             value={description}
             onChange={handleChange}
           />
-        </label>
-        <button className="border">Create Anime!</button>
+        <button type='submit' className=""
+          onClick={handleSubmit}>Save</button>
       </form>
     </div>
   )
