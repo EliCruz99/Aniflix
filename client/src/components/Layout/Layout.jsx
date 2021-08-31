171915235a57c018f0a0868ef4b36c38e0206028
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 09:23:03 -0700

    fixed minor text error

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout(props) {
   const { currentUser, handleLogout } = props
 
   return (
-    <header className=' bg-palette-backgroundLight h-full'>
+    <header className=' bg-palette-backgroundLight w-full h-full'>
       <Nav currentUser={currentUser} handleLogout={handleLogout}/>
       {props.children}
       <Footer/>
