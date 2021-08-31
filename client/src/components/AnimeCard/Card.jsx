c02ed6047ab3e4c831b3f502ad91cf75187d0069
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 12:52:17 -0700

    working on media queries

--- client/src/components/AnimeCard/Card.jsx
+++ client/src/components/AnimeCard/Card.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Card({anime}) {
   return (
-    <div className='flex w-72 h-32 border-2'>
-      <img src={anime.img_url} alt={anime.name} className="border-r-2 w-32 h-32" />
+    <div className='flex w-72 h-32 border-2 '>
+      <img src={anime.img_url} alt={anime.name} className=" w-32 h-32" />
       <div className="flex flex-col ">
         <span className=" border-b-2">{anime.name}</span>
         <p className="border-b-2">{anime.category?.name}</p>
