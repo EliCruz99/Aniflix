b71dea1afa784ea48059b9c9c6681b88d357762d
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 09:24:17 -0700

    fixed issue with dropdown bar

--- client/src/screens/AnimeCategory/AnimeCategory.jsx
+++ client/src/screens/AnimeCategory/AnimeCategory.jsx
@@ -11,7 +11,7 @@ export default function AnimeCategory({allCategories, allAnimes}) {
     console.log(category)
   }
   
-  const categories = allCategories.map((cat) => <option value={cat.name}>{cat.name}</option> )
+  const categories = allCategories.map((cat) => <option key={cat.id} value={cat.name}>{cat.name}</option> )
   const filtered = allAnimes.filter((anime) => anime.category.name === category)
   const shows = filtered.map((anime) => (<Link key={anime.id} to={`/anime/${anime.id}`} className='m-10'>{<Card anime={anime}/>}</Link>))
 
@@ -23,14 +23,16 @@ export default function AnimeCategory({allCategories, allAnimes}) {
   }
 
   return (
-    <>
+   <>
       <select value={category} onChange={handleChange}>
         <option>All</option>
         {categories}
       </select>
+    <div className='flex justify-center flex-wrap'>
       <div className='flex flex-wrap'>
         {categoryToAnime()}
       </div>
+      </div>
       </>
-  )
+      )
 }
