c2e5690546bf45487ebaefab807a685e56b04740
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 09:26:15 -0700

    fixed styling

--- client/src/screens/Created/Created.jsx
+++ client/src/screens/Created/Created.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Card from '../../components/AnimeCard/Card'
 
 export default function Created({ currentUser, allAnimes }) {
 
   const userAnime = allAnimes.filter((anime) => anime?.user_id === currentUser?.id)
 
   return (
-    <div>
+    <div className=''>
       <p className="">{currentUser?.username}</p>
+      <div className="flex flex-wrap justify-center">
       {userAnime.map((anime) => {
         return (
-          <Link to={`/anime/${anime.id}`}><div className="">
-            <p className="">{anime.name}</p>
-            <img src={anime.img_url} alt="" className="" />
-          </div></Link>
+          <Link to={`/anime/${anime.id}`} className='m-3'>
+            <Card anime={anime}/></Link>
         )
       })}
+      </div>
     </div>
   )
 }
