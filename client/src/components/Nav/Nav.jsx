dc0d62dfdfd726838b500a8215d726320d2dc7cb
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-31 20:57:34 -0700

    finalizing for deployment

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Search from '../Search/Search'
 
 export default function Nav(props) {
   const {currentUser, handleLogout} = props
 
   return (
   <div className='z-10 bg-palette-darkCard shadow-lg'>
-      <nav className='flex xl:flex-row xl:justify-evenly xl:flex-nowrap sm:align-baseline'>
+      <nav className='flex xl:flex-row xl:justify-evenly xl:flex-nowrap align-baseline'>
         <Link className='mr-1 text-palette-font text-2xl' to='/home'>Aniflix</Link>
         <Link to='/category' className='ml-1 text-palette-font'>Categories</Link>
       {currentUser ? (
@@ -26,7 +25,6 @@ export default function Nav(props) {
             <Link to='/anime/new' className="ml-1 text-palette-font" >Make new Anime</Link>
           </div>
         )}
-        <Search/>
           </nav>
     </div>
   )
