f4389f90c144457d682b3e3dee6a43de1138648a
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-11-02 14:51:23 -0700

    finished remaking carousel

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 export default function Carousel({shuffle}) {
   const [currentImg, setCurrentImg] = useState(0)
@@ -21,12 +22,12 @@ export default function Carousel({shuffle}) {
 
   return (
     <section className='relative flex justify-center content-center'>
-      <FaArrowAltCircleLeft onClick={prevSlide} className='absolute top-2/4 inset-y-0 -left-8 text-5xl z-10 cursor-pointer select-none'/>
-      <FaArrowAltCircleRight onClick={nextSlide} className='absolute top-2/4 inset-y-0 -right-8 text-5xl z-10 cursor-pointer select-none'/>
+      <FaArrowAltCircleLeft onClick={prevSlide} className='absolute top-2/4 inset-y-0 -left-8 text-4xl z-10 cursor-pointer select-none'/>
+      <FaArrowAltCircleRight onClick={nextSlide} className='absolute top-2/4 inset-y-0 -right-8 text-4xl z-10 cursor-pointer select-none'/>
       {shuffle?.slice(16, 25).map((anime, index) => {
         return (
           <div key={index} className={index === currentImg ? 'opacity-1 duration-100 transform scale-90': 'opacity-0 duration-100'}>
-            {index === currentImg && (<img  src={anime.img_url} alt={anime.name} className='rounded' />)}
+            {index === currentImg && (<Link to={`/anime/${anime.id}`}><img src={anime.img_url} alt={anime.name} className='rounded' /><p>{anime.name}</p></Link>)}
             
           </div>
         )
