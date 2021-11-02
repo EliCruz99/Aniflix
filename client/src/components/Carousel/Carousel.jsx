c84e08dae0885dca05e20c06279bc5af07fa2bba
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-11-02 14:32:04 -0700

    remade carousel since original package leaked imgs on loadup

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 
-export default function Carousel() {
+export default function Carousel({shuffle}) {
+  const [currentImg, setCurrentImg] = useState(0)
+  const length = shuffle.slice(16,25).length
 
 
-  
+  const nextSlide = () => {
+    setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrentImg( currentImg === 0? length - 1: currentImg - 1)
+  }
+
+
+  if (!Array.isArray(shuffle) || shuffle.length <= 0) {
+    return null
+  }
+
   return (
-    <div>
-      
-    </div>
+    <section className='relative flex justify-center content-center'>
+      <FaArrowAltCircleLeft onClick={prevSlide} className='absolute top-2/4 left-1 text-5xl z-10 cursor-pointer select-none'/>
+      <FaArrowAltCircleRight onClick={nextSlide} className='absolute top-2/4 right-1 text-5xl z-10 cursor-pointer select-none'/>
+      {shuffle?.slice(16, 25).map((anime, index) => {
+        return (
+          <div key={index} className={index === currentImg ? 'slide active': 'slide'}>
+            {index === currentImg && (<img  src={anime.img_url} alt={anime.name} className='rounded' />)}
+            
+          </div>
+        )
+        
+  })}
+    </section>
   )
 }
